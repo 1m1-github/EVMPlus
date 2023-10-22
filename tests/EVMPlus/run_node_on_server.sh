c78543de3ba2d7db0776190df7b688e1c20506ed
1m1 <email@1m1.io>
2023-10-21 17:40:29 -0700

    enr of bootnode

--- tests/EVMPlus/run_node_on_server.sh
+++ tests/EVMPlus/run_node_on_server.sh
@@ -19,12 +19,15 @@ tmux new -s geth
 ./build/bin/geth account new --datadir ~/chaindata
 // add public address to genesis.json following https://geth.ethereum.org/docs/fundamentals/private-network#clique-example
 ./build/bin/geth init --datadir ~/chaindata ./tests/EVMPlus/genesis.json
-./build/bin/geth --http --http.api web3,eth,net --datadir ~/chaindata --networkid 196790 --nat extip:35.209.100.125
+./build/bin/geth --datadir ~/chaindata --networkid 196790 --nat extip:35.209.100.125
 
 // remember to open HTTP port, any other?
 
+./build/bin/geth attach --exec admin.nodeInfo.enr ~/chaindata/geth.ipc
+enr:-KO4QCnVVlXYZrs6y8EIOjUlR6pqtO0BAkLxKQxvW3B72muNCmbpnqrXx8KOGcoBsyWsiCJW9h32uTe7SHMmejkFRG2GAYtUsPIFg2V0aMfGhDgzR7mAgmlkgnY0gmlwhCPRZH2Jc2VjcDI1NmsxoQNU9riSVzaP3AwzmhA3-GH78YP7TTFwo_KP70rB5BWZJIRzbmFwwIN0Y3CCdl-DdWRwgnZf
 
-./build/bin/geth init --datadir ~/Downloads/chaindata ./tests/EVMPlus/genesis.json
-./build/bin/geth --http --http.api web3,eth,net --datadir ~/Downloads/chaindata --networkid 196790 --vmdebug --dev --nat extip:35.209.100.125
-./build/bin/geth --datadir ~/Downloads/chaindata --networkid 196790 --nat extip:35.209.100.125
+./build/bin/geth init --datadir ~/Downloads/chaindata1 ./tests/EVMPlus/genesis.json
+./build/bin/geth --datadir ~/Downloads/chaindata1 --networkid 196790 --port 30303 --bootnodes enr:-KO4QCnVVlXYZrs6y8EIOjUlR6pqtO0BAkLxKQxvW3B72muNCmbpnqrXx8KOGcoBsyWsiCJW9h32uTe7SHMmejkFRG2GAYtUsPIFg2V0aMfGhDgzR7mAgmlkgnY0gmlwhCPRZH2Jc2VjcDI1NmsxoQNU9riSVzaP3AwzmhA3-GH78YP7TTFwo_KP70rB5BWZJIRzbmFwwIN0Y3CCdl-DdWRwgnZf --authrpc.port 8551
+./build/bin/geth --datadir ~/Downloads/chaindata2 --networkid 196790 --port 30304 --bootnodes enr:-KO4QCnVVlXYZrs6y8EIOjUlR6pqtO0BAkLxKQxvW3B72muNCmbpnqrXx8KOGcoBsyWsiCJW9h32uTe7SHMmejkFRG2GAYtUsPIFg2V0aMfGhDgzR7mAgmlkgnY0gmlwhCPRZH2Jc2VjcDI1NmsxoQNU9riSVzaP3AwzmhA3-GH78YP7TTFwo_KP70rB5BWZJIRzbmFwwIN0Y3CCdl-DdWRwgnZf --authrpc.port 8552
 
+./build/bin/geth attach ~/Downloads/chaindata1/geth.ipc
