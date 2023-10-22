baf2a97f49ef36e2ea1a4f81595a79a686c98c76
1m1 <email@1m1.io>
2023-10-21 19:43:25 -0700

    ethhash genenis

--- tests/EVMPlus/run_node_on_server.sh
+++ tests/EVMPlus/run_node_on_server.sh
@@ -21,21 +21,18 @@ tmux new -s geth
 ./build/bin/geth account new --datadir ~/chaindata
 // add public address to genesis.json following https://geth.ethereum.org/docs/fundamentals/private-network#clique-example
 ./build/bin/geth init --datadir ~/chaindata ./tests/EVMPlus/genesis.json
-./build/bin/geth --datadir ~/chaindata --networkid 196790 --port 30313 --nat extip:35.209.100.125 --unlock 0x58c4c45c9b5954ee15E81C0FB7437DCaCEAD665e --mine
+./build/bin/geth --datadir ~/chaindata --networkid 196790 --port 30313 --nat extip:35.209.100.125 --unlock 0x58c4c45c9b5954ee15E81C0FB7437DCaCEAD665e --password ~/chaindata/password.txt --mine --miner.etherbase=0x58c4c45c9b5954ee15E81C0FB7437DCaCEAD665e
 
 // remember to open HTTP port, any other?
 
 ./build/bin/geth attach --exec admin.nodeInfo.enr ~/chaindata/geth.ipc
-enr:-KO4QADz4jFPdEhiGCd7NRkYqF4E_RZVWJzWiyQh33NrB-IJBzzsSy22EmyqcLW9uZH8bpA1DqpjQtsLvUR0eS7yrZiGAYtVJttTg2V0aMfGhKkY9ZOAgmlkgnY0gmlwhCPRZH2Jc2VjcDI1NmsxoQMbb-flS4GLsP7tkVEDFltsiJnKszXtgKU3uVRh4EkJ34RzbmFwwIN0Y3CCdmmDdWRwgnZp
-
-// cliqiue mining
-./build/bin/geth --unlock 0x58c4c45c9b5954ee15E81C0FB7437DCaCEAD665e --mine
+enr:-KO4QIzzViglXefgjUBy1A2V_t1BrjZ1dgcn8Lu2Yes0ZUjBf-QJ7bp47KVUQil_WVtA89idZCnluKLkffQ0Ns0czYWGAYtVNkH0g2V0aMfGhKHCtOKAgmlkgnY0gmlwhCPRZH2Jc2VjcDI1NmsxoQNZ5N2y4Z2Ehb7UQVuKC24a1CWntX4b2OEBesNFVnaX9IRzbmFwwIN0Y3CCdmmDdWRwgnZp
 
 // member node steps
 
 ./build/bin/geth account new --datadir ~/chaindata
 ./build/bin/geth init --datadir ~/chaindata ./tests/EVMPlus/genesis.json
-./build/bin/geth --datadir ~/chaindata --networkid 196790 --port 30313 --http --http.api 'personal,eth,net,web3' --bootnodes enr:-KO4QADz4jFPdEhiGCd7NRkYqF4E_RZVWJzWiyQh33NrB-IJBzzsSy22EmyqcLW9uZH8bpA1DqpjQtsLvUR0eS7yrZiGAYtVJttTg2V0aMfGhKkY9ZOAgmlkgnY0gmlwhCPRZH2Jc2VjcDI1NmsxoQMbb-flS4GLsP7tkVEDFltsiJnKszXtgKU3uVRh4EkJ34RzbmFwwIN0Y3CCdmmDdWRwgnZp
+./build/bin/geth --datadir ~/chaindata --networkid 196790 --port 30313 --unlock 0xb316c8ca80e0dce73f3a81338ed97f31fe0a31eb --password ~/chaindata/password.txt --bootnodes enr:-KO4QIzzViglXefgjUBy1A2V_t1BrjZ1dgcn8Lu2Yes0ZUjBf-QJ7bp47KVUQil_WVtA89idZCnluKLkffQ0Ns0czYWGAYtVNkH0g2V0aMfGhKHCtOKAgmlkgnY0gmlwhCPRZH2Jc2VjcDI1NmsxoQNZ5N2y4Z2Ehb7UQVuKC24a1CWntX4b2OEBesNFVnaX9IRzbmFwwIN0Y3CCdmmDdWRwgnZp
 ./build/bin/geth attach ~/chaindata/geth.ipc
 
 // write password from account new step into file
